865fafb4f6a4fb4420a301c8523446f52317831d
D3vmotiv <czechpiotr2701@gmail.com>
2021-12-26 15:02:02 +0100

    Read message

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { createBot } from "./init.js";
 
 const bot = createBot();
+const msgPrefix = process.env.MESSAGE_PREFIX;
 
 bot.once("ready", () => {
-  console.log(321);
+  console.log("Bot start running");
+});
+
+bot.on("messageCreate", (message) => {
+  console.log("Bot got message");
+
+  const { author, content, channel } = message;
+
+  if (author.bot || !content.startsWith(msgPrefix)) {
+    return;
+  }
+
+  const [commandName, ...commandContent] = content
+    .slice(msgPrefix.length)
+    .split(/ +/);
 });
