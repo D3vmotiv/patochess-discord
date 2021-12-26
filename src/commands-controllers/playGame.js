a07eeb9322481ed6befdc4c507e38f1918419c01
D3vmotiv <czechpiotr2701@gmail.com>
2021-12-26 15:47:57 +0100

    Delete unnecessary console logs

--- src/commands-controllers/playGame.js
+++ src/commands-controllers/playGame.js
@@ -1,21 +1,17 @@
 const isTimeInCorrectFormat = (time, maxNumber = 59) => {
   if (Number.isNaN(time)) {
-    console.log(1);
     return false;
   }
 
   if (time < 1) {
-    console.log(2);
     return false;
   }
 
   if (!Number.isInteger(time)) {
-    console.log(3);
     return false;
   }
 
   if (time > maxNumber) {
-    console.log(5);
     return false;
   }
 
