772a2cd385b7f503247150afce59d441aabf85dd
D3vmotiv <czechpiotr2701@gmail.com>
2021-12-26 20:02:24 +0100

    add es6 imports in test files

---
+++ __tests__/init.spec.js
@@ -0,0 +1,11 @@
+import { createBot } from "./../src/init.js";
+
+describe("Init functions", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return bot object", () => {
+    expect(1).toBe(1);
+  });
+});
