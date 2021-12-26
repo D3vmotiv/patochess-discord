657c58527b7d7bd593fd964fcfa343d0261d6acd
D3vmotiv <czechpiotr2701@gmail.com>
2021-12-26 19:17:56 +0100

    Jest setup

--- src/init.js
+++ src/init.js
@@ -1,9 +1,6 @@
 import { Client, Intents } from "discord.js";
-import dotenv from "dotenv";
 
 const createBot = () => {
-  dotenv.config();
-
   const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
   });
